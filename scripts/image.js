56f2aee88d3cad461b7e89b6d3d400d412b2b474
slikts <dabas@untu.ms>
2020-05-09 20:13:43 +0300

    format

--- scripts/image.js
+++ scripts/image.js
@@ -1,24 +1,26 @@
-const { argv } = require('yargs')
-const globby = require('globby')
+const { argv } = require('yargs');
+const globby = require('globby');
 const sharp = require('sharp');
 
-const { generateOre } = require('./ore')
-const { makeIcon, makeIconSet } = require('./icon')
+const { generateOre } = require('./ore');
+const { makeIcon, makeIconSet } = require('./icon');
 
 const main = async (argv) => {
-  const { _: [ action ] } = argv;
+  const {
+    _: [action],
+  } = argv;
 
   if (action === 'icon') {
     makeIcon({ argv });
   } else if (action === 'iconset') {
     makeIconSet(argv._[1]);
   } else if (action === 'ore') {
-    generateOre({ argv })
+    generateOre({ argv });
   }
-}
+};
 
 try {
-  main(argv)
-} catch(e) {
+  main(argv);
+} catch (e) {
   console.error(e);
 }
