8afb3f26113d8c8972bf1c4a3571d5a7b322a582
slikts <dabas@untu.ms>
2020-05-10 14:13:48 +0300

    fix iron icon

--- src/prototypes/emoji-ore.lua
+++ src/prototypes/emoji-ore.lua
@@ -1,6 +1,7 @@
 local util = require('util')
 
-local filenames = util.concat({ util.icon('emoji-ore') }, util.map(function(i)
+local icon = util.icon('emoji-ore')
+local filenames = util.concat({ icon }, util.map(function(i)
   return util.icon('emoji-ore-' .. i)
 end, util.range(1, 15)))
 local pictures = util.map(function(name)
